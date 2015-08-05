569abd40c7c2b366802d47f160483861b5d9b37f
Koala00 <fabrice_m.leger@laposte.net>
2015-08-05 21:08:30 +0200

    colors shaded

--- Assets/Scripts/ConfigurationElements.cs
+++ Assets/Scripts/ConfigurationElements.cs
@@ -9,8 +9,11 @@ class ConfigurationElements
     public static int board_size_x = 29;
     public static int board_size_z = 29;
 
-    public static int towers_reachDistance = 1;
-    public static int ControlDistance = 1;
+    public static int towers_reachDistance = 3;
+    public static int ControlDistance = 3;
 
-    public static Color[] playersColor = { Color.red, Color.blue } ;
+    public static Color[] playersColor = {
+        Color.Lerp(Color.white, Color.red, 0.5f),
+        Color.Lerp(Color.white, Color.blue, 0.5f),
+    } ;
 }
